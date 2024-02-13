c9a37195ded8a543a4a417180c4d8bbaa0d1b35f
yokouchi_kouta <115063415+yokouchikouta@users.noreply.github.com>
2024-02-13 14:11:08 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include <thread>
+
+void thread1() {
+	printf("thread1\n");
+}
+
+void thread2() {
+	printf("thread2\n");
+}
+
+void thread3() {
+	printf("thread3\n");
+}
+
+int main() {
+	std::thread th1(thread1);
+	th1.join();
+
+	std::thread th2(thread2);
+	th2.join();
+
+	std::thread th3(thread3);
+	th3.join();
+
+
+	return 0;
+}
